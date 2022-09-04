d2ea76eeaae1b3bef060610d1fa22bce8aaf9db3
JuanRodriguez507 <112440976+JuanRodriguez507@users.noreply.github.com>
2022-09-04 12:12:05 -0500

    Create lab26.php

---
+++ lab2/lab26.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	$a = 5;
+	$b = "5";
+
+	echo ($a == 5) ? "Verdadero" : "Falso", "<br>";
+
+	echo ($a === 5) ? "Verdadero" : "Falso", "<br>";
+
+	echo ($b == 5) ? "Verdadero" : "Falso", "<br>";
+
+	echo ($b === 5) ? "Verdadero" : "Falso", "<br>";
+
+	?>
+</body>
+</html>	
+
