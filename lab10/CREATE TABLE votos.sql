e08aede1f2ca355c0bb4b971098bb8178bafec38
JuanRodriguez507 <112440976+JuanRodriguez507@users.noreply.github.com>
2022-10-12 21:03:40 -0500

    Update CREATE TABLE votos.sql

--- lab10/CREATE TABLE votos.sql
+++ lab10/CREATE TABLE votos.sql
@@ -4,3 +4,33 @@ votos1 int (10) unsigned NOT NULL  default '0',
 votos2 int (10) unsigned NOT NULL  default '0',
 PRIMARY KEY (id)
 );
+
+
+
+INSERT INTO votos VALUE (1, 49, 12);
+
+
+
+
+CREATE DEFINER = `root`@`localhost` PROCEDURE `sp_listar_votos` ()
+BEGIN
+ SELECT votos1,votos2 FROM votos;
+END;
+
+
+   
+CREATE DEFINER = `root`@`localhost` PROCEDURE `sp_actualizar_votos` (
+IN  param_votos1 VARCHAR (255),
+IN  param_votos2 VARCHAR (255)
+)
+BEGIN
+  
+  SET @S = CONCAT("UPDATE votos SET votos1=", param_votos1 ,", votos2=", param_votos2);
+
+  PREPARE stmt FROM @S;
+  EXECUTE stmt;
+  DEALLOCATE PREPARE stmt;
+
+END;
+
+
