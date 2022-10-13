04cbc1f71230e9f49ce9c70ead5f7776864fc4b1
JuanRodriguez507 <112440976+JuanRodriguez507@users.noreply.github.com>
2022-10-12 21:56:19 -0500

    lab13

---
+++ lab13/lab133.php
@@ -0,0 +1,18 @@
+<HTML LANG="es">
+<HEAD>
+  <TITLE>Laboratorio 13 </TITLE>
+  <LINK REL="stylesheet" TYPE="text/css" HREF="css/estilo.css">
+  </HEAD>
+<BODY>
+  <H1>Recupera valor de una Cookie</H1>
+   <?PHP
+if 
+  (isset($_COOKIE["user"]))
+echo "<H2>Bienvenido invitado!</H2><br/>";
+?>
+<A HREF='lab131.php'>...Regresar</A>&nbsp;
+<A HREF='lab134.php'>Continuar....</A>
+</BODY>
+</HTML>
+
+
